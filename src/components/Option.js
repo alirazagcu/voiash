4c1a96c0868968f33fce5f330b9ba544052aae8b
Muhammad Ahsan <Muhammad.Ahsan@staples.com>
2021-07-23 18:44:15 +0500

    integrate experiences in selection

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,171 +1,110 @@
-import React from 'react';
-import '../App.css';
-import {Link} from 'react-router-dom';
-import { Col, Card, Row, Container, Carousel } from 'react-bootstrap'
-import pics from '../images/party.jpg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-function Option() {
+import React, { useEffect, useState } from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
+import { Col, Card, Row, Container, Carousel } from "react-bootstrap";
+import pics from "../images/party.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  experiences,
+  experienceStateClear,
+  selectedExperience,
+} from "../store/experienceReducer";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "./material-ui-comps/Loader";
+import SnackBar from "./material-ui-comps/SnackBar";
+import { isEmpty } from "lodash";
 
-    return (
-        <div className="App1">
-            <div className="grid1">
-                <div class="row rowwww">
-                    <div class="col-sm-6 col-xs-12 col-md-3 col-lg-3 columy">
-                        <Link to="/experiencies1">
-                        <Card className="columys1" border="secondary" border-radius="50">
-                            <img src={pics} className="bgimage"/>
-                            <div class="textforcard">
-                                <Card.Body>
-                                    <Card.Title> Mandala Disco Bottle Service (Diamond)</Card.Title>
-                                    <Card.Text>
-                                        5 horas
-                         </Card.Text>
-                                    <div class="textforcards1">
-                                        <Card.Title>Desde: 55.00 USD</Card.Title>
-                                        <Card.Text>
-                                            todo incluido por persona, precio total.
-                         </Card.Text>
-                                    </div>
-                                </Card.Body>
-                            </div>
-                        </Card></Link></div>
-                        <div class="col-sm-6 col-xs-12 col-md-3 col-lg-3 columy">
-                        <Link to="/experiencies1">
-                        <Card className="columys1" border="secondary" border-radius="50">
-                            <img src={pics} className="bgimage"/>
-                            <div class="textforcard">
-                                <Card.Body>
-                                    <Card.Title> Mandala Disco Bottle Service (Diamond)</Card.Title>
-                                    <Card.Text>
-                                        5 horas
-                         </Card.Text>
-                                    <div class="textforcards1">
-                                        <Card.Title>Desde: 55.00 USD</Card.Title>
-                                        <Card.Text>
-                                            todo incluido por persona, precio total.
-                         </Card.Text>
-                                    </div>
-                                </Card.Body>
-                            </div>
-                        </Card></Link></div>
-                        <div class="col-sm-6 col-xs-12 col-md-3 col-lg-3 columy">
-                        <Link to="/experiencies1">
-                        <Card className="columys1" border="secondary" border-radius="50">
-                            <img src={pics} className="bgimage"/>
-                            <div class="textforcard">
-                                <Card.Body>
-                                    <Card.Title> Mandala Disco Bottle Service (Diamond)</Card.Title>
-                                    <Card.Text>
-                                        5 horas
-                         </Card.Text>
-                                    <div class="textforcards1">
-                                        <Card.Title>Desde: 55.00 USD</Card.Title>
-                                        <Card.Text>
-                                            todo incluido por persona, precio total.
-                         </Card.Text>
-                                    </div>
-                                </Card.Body>
-                            </div>
-                        </Card></Link></div>
-                        <div class="col-sm-6 col-xs-12 col-md-3 col-lg-3 columy">
-                        <Link to="/experiencies1">
-                        <Card className="columys1" border="secondary" border-radius="50">
-                            <img src={pics} className="bgimage"/>
-                            <div class="textforcard">
-                                <Card.Body>
-                                    <Card.Title> Mandala Disco Bottle Service (Diamond)</Card.Title>
-                                    <Card.Text>
-                                        5 horas
-                         </Card.Text>
-                                    <div class="textforcards1">
-                                        <Card.Title>Desde: 55.00 USD</Card.Title>
-                                        <Card.Text>
-                                            todo incluido por persona, precio total.
-                         </Card.Text>
-                                    </div>
-                                </Card.Body>
-                            </div>
-                        </Card></Link></div>
-                        <div class="col-sm-6 col-xs-12 col-md-3 col-lg-3 columy">
-                        <Link to="/experiencies1">
-                        <Card className="columys1" border="secondary" border-radius="50">
-                            <img src={pics} className="bgimage"/>
-                            <div class="textforcard">
-                                <Card.Body>
-                                    <Card.Title> Mandala Disco Bottle Service (Diamond)</Card.Title>
-                                    <Card.Text>
-                                        5 horas
-                         </Card.Text>
-                                    <div class="textforcards1">
-                                        <Card.Title>Desde: 55.00 USD</Card.Title>
-                                        <Card.Text>
-                                            todo incluido por persona, precio total.
-                         </Card.Text>
-                                    </div>
-                                </Card.Body>
-                            </div>
-                        </Card></Link></div>
-                        <div class="col-sm-6 col-xs-12 col-md-3 col-lg-3 columy">
-                        <Link to="/experiencies1">
-                        <Card className="columys1" border="secondary" border-radius="50">
-                            <img src={pics} className="bgimage"/>
-                            <div class="textforcard">
-                                <Card.Body>
-                                    <Card.Title> Mandala Disco Bottle Service (Diamond)</Card.Title>
-                                    <Card.Text>
-                                        5 horas
-                         </Card.Text>
-                                    <div class="textforcards1">
-                                        <Card.Title>Desde: 55.00 USD</Card.Title>
-                                        <Card.Text>
-                                            todo incluido por persona, precio total.
-                         </Card.Text>
-                                    </div>
-                                </Card.Body>
-                            </div>
-                        </Card></Link></div>
-                        <div class="col-sm-6 col-xs-12 col-md-3 col-lg-3 columy">
-                        <Link to="/experiencies1">
-                        <Card className="columys1" border="secondary" border-radius="50">
-                            <img src={pics} className="bgimage"/>
-                            <div class="textforcard">
-                                <Card.Body>
-                                    <Card.Title> Mandala Disco Bottle Service (Diamond)</Card.Title>
-                                    <Card.Text>
-                                        5 horas
-                         </Card.Text>
-                                    <div class="textforcards1">
-                                        <Card.Title>Desde: 55.00 USD</Card.Title>
-                                        <Card.Text>
-                                            todo incluido por persona, precio total.
-                         </Card.Text>
-                                    </div>
-                                </Card.Body>
-                            </div>
-                        </Card></Link></div>
-                        <div class="col-sm-6 col-xs-12 col-md-3 col-lg-3 columy">
-                        <Link to="/experiencies1">
-                        <Card className="columys1" border="secondary" border-radius="50">
-                            <img src={pics} className="bgimage"/>
-                            <div class="textforcard">
-                                <Card.Body>
-                                    <Card.Title> Mandala Disco Bottle Service (Diamond)</Card.Title>
-                                    <Card.Text>
-                                        5 horas
-                         </Card.Text>
-                                    <div class="textforcards1">
-                                        <Card.Title>Desde: 55.00 USD</Card.Title>
-                                        <Card.Text>
-                                            todo incluido por persona, precio total.
-                         </Card.Text>
-                                    </div>
-                                </Card.Body>
-                            </div>
-                        </Card></Link></div>
-                </div >
-            </div>
+function Option() {
+  const dispatch = useDispatch();
+  const { isError, isFetching, isSuccess, msg, responseData } = useSelector(
+    (state) => state.experienceState
+  );
+  const [open, setOpen] = React.useState(false);
+  const [totalAmount, setTotalAmount] = useState(0);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    dispatch(experiences({ type: "get", token: token }));
+  }, []);
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(experienceStateClear());
+    }
+    if (isError) {
+      setOpen(true);
+      dispatch(experienceStateClear());
+    }
+  }, [isSuccess, isError]);
+
+  useEffect(() => {
+    if (!isEmpty(responseData)) {
+      let totalAmount1 = 0;
+      responseData.map((price) => {
+        price.prices.map((data) => {
+          if (data.amount) {
+            totalAmount1 += parseInt(data.amount);
+          }
+        });
+      });
+      setTotalAmount(totalAmount1);
+    }
+  }, [responseData]);
+
+  const experienceClickHandler = (row) => {
+    dispatch(selectedExperience(row));
+  };
+
+  return isEmpty(responseData) ? (
+    <div style={{ marginTop: "300px" }}>
+      <Loader />
+    </div>
+  ) : (
+    <div className="App1">
+      <div className="grid">
+        <div class="row rowwww">
+          {!isEmpty(responseData) &&
+            responseData.map((exp) => {
+              return (
+                <div
+                  class="col-sm-6 col-xs-12 col-md-3 col-lg-3 columy"
+                  key={exp._id}
+                >
+                  <Link
+                    to="/experiencies1"
+                    onClick={() => experienceClickHandler(exp)}
+                  >
+                    <Card
+                      className="columy1"
+                      border="secondary"
+                      border-radius="50"
+                    >
+                      <img
+                        src={exp.listImage.imageLink || pics}
+                        className="bgimage"
+                      />
+                      <div class="textforcard">
+                        <Card.Body>
+                          <Card.Title> {exp.name}</Card.Title>
+                          <Card.Text>5 horas</Card.Text>
+                          <div class="textforcard1">
+                            <Card.Title>
+                              Desde: {totalAmount} {exp.prices[0].coin}
+                            </Card.Title>
+                            <Card.Text>
+                              todo incluido por persona, precio total.
+                            </Card.Text>
+                          </div>
+                        </Card.Body>
+                      </div>
+                    </Card>
+                  </Link>
+                </div>
+              );
+            })}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Option;
